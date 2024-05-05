09e20ec5fe5f701d75c9f290c0c2d1a5da9091fd
kandeel <youssifk000@gmail.com>
2024-05-05 19:05:00 +0300

    adding global var

--- main.py
+++ main.py
@@ -5,36 +5,41 @@ import rsa
 public_key, private_key = rsa.newkeys(1024)
 public_parter = None
 
-
-
 def sending_messages(sock):
     while True:
         message = input("")
-        sock.send(rsa.encrypt(message.encode(),public_parter))
+        encrypted_message = rsa.encrypt(message.encode(), public_parter)
+        sock.send(encrypted_message)
         print("You: " + message)
 
 def receiving_messages(sock):
     while True:
-        print("Partner: " + rsa.decrypt(sock.recv(1024),private_key))
+        encrypted_message = sock.recv(1024)
+        decrypted_message = rsa.decrypt(encrypted_message, private_key)
+        print("Partner: " + decrypted_message.decode())
 
 def start_server():
+    global public_parter  # Make sure to use the global variable
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("192.168.1.6", 9999))
     server.listen()
 
     client, _ = server.accept()
-    public_parter=rsa.PublicKey.load_pkcs1(client.recv(1024))
     client.send(public_key.save_pkcs1("PEM"))
+    received_public_key = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    public_parter = received_public_key
     print("Connected to client")
 
     threading.Thread(target=sending_messages, args=(client,)).start()
     threading.Thread(target=receiving_messages, args=(client,)).start()
 
 def start_client():
+    global public_parter  # Make sure to use the global variable
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect(("192.168.1.6", 9999))
-    public_parter=rsa.PublicKey.load_pkcs1(server.recv(1024))
+    received_public_key = rsa.PublicKey.load_pkcs1(server.recv(1024))
     server.send(public_key.save_pkcs1("PEM"))
+    public_parter = received_public_key
     print("Connected to server")
 
     threading.Thread(target=sending_messages, args=(server,)).start()
